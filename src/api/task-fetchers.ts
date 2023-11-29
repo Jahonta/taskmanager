29deb42ec43001ffcbfbf13d4494f1f43cf647c5
Lydia Zakharova <jahontis@gmail.com>
2023-11-29 15:59:31 +0300

    fix(api): Типизирует фетчеры

--- src/api/task-fetchers.ts
+++ src/api/task-fetchers.ts
@@ -4,19 +4,19 @@ import { adaptToServer } from './adapters/client-to-server';
 import { adaptToClient } from './adapters/server-to-client';
 import { request } from './helpers/request';
 
-function getTasks() {
+function getTasks(): Promise<Task[]> {
   return request().then((tasks) => {
     return tasks.map(adaptToClient);
   });
 }
 
-function addTask(task: Omit<Task, 'id'>) {
+function addTask(task: Omit<Task, 'id'>): Promise<Task> {
   return request({ method: 'POST', body: adaptToServer(task) }).then((task) => {
     return adaptToClient(task);
   });
 }
 
-function updateTask(task: Task) {
+function updateTask(task: Task): Promise<Task> {
   return request({
     method: 'PUT',
     id: task.id,
@@ -26,7 +26,7 @@ function updateTask(task: Task) {
   });
 }
 
-function deleteTask(task: Task) {
+function deleteTask(task: Task): Promise<void> {
   return request({ method: 'DELETE', id: task.id });
 }
 
