29d265f2d81c308abdce379809192a046dd849d4
Lydia Zakharova <jahontis@gmail.com>
2023-12-08 16:27:36 +0300

    feat(api): Добавит кэширование

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,4 @@
 export const TASKS_API = 'https://21.objects.pages.academy/task-manager/tasks';
 export const API_TOKEN = import.meta.env.VITE_API_TOKEN;
 export const QUERY_KEY = 'tasks';
+export const CASH_TIME = 1000 * 5;
--- src/api/hooks/use-tasks.ts
+++ src/api/hooks/use-tasks.ts
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { QUERY_KEY } from '../constants';
+import { QUERY_KEY, CASH_TIME } from '../constants';
 import { getTasks } from '../task-fetchers';
 
 function useTasks() {
-  return useQuery({ queryKey: [QUERY_KEY], queryFn: getTasks });
+  return useQuery({
+    queryKey: [QUERY_KEY],
+    queryFn: getTasks,
+    staleTime: CASH_TIME,
+  });
 }
 
 export { useTasks };
