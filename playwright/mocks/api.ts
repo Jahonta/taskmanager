3be5d6324805dc8c4469da6f3882dff6dc596c26
Lydia Zakharova <jahontis@gmail.com>
2023-12-23 14:53:22 +0300

    feat(test): Добавит имитацию crud-сервера

--- playwright/mocks/api.ts
+++ playwright/mocks/api.ts
@@ -1,13 +1,48 @@
+import crypto from 'node:crypto';
 import { Page } from '@playwright/test';
 
 import { mockTasks } from './tasks';
 
 export async function mockAPI(page: Page, isEmpty: boolean = false) {
-  const tasks = isEmpty ? [] : mockTasks;
-  await page.route('*/**/task-manager/tasks', async (route) => {
-    if (route.request().method() === 'GET') {
-      await route.fulfill({ json: tasks });
+  let tasks = isEmpty ? [] : [...mockTasks];
+  await page.route(/task-manager\/tasks/, async (route) => {
+    if (!route.request().headers().authorization?.startsWith('Basic ')) {
+      await route.fulfill({ status: 401 });
       return;
+    }
+
+    switch (route.request().method()) {
+      case 'GET': {
+        await route.fulfill({ json: tasks });
+        return;
+      }
+      case 'POST': {
+        const newTask = {
+          ...route.request().postDataJSON(),
+          id: crypto.randomUUID(),
+        };
+        tasks.unshift(newTask);
+        await route.fulfill({ json: newTask });
+        return;
+      }
+      case 'DELETE': {
+        tasks = tasks.filter(
+          (task) => task.id !== route.request().url().split('/').pop()
+        );
+        await route.fulfill({ status: 204 });
+        return;
+      }
+      case 'PUT': {
+        const updatedTask = {
+          ...route.request().postDataJSON(),
+          id: route.request().url().split('/').pop(),
+        };
+        tasks = tasks.map((task) =>
+          task.id === updatedTask.id ? updatedTask : task
+        );
+        await route.fulfill({ json: updatedTask });
+        return;
+      }
     }
   });
 }
